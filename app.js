8eb80ee8cc56c6570ce6c4ee9a80f9533c63c5fe
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-13 19:33:29 +0330

    dispatching actions

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const initialState = {
 };
 
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
 
 const store = createStore(rootReducer);
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "ADD", value: 2 });
 
 console.log(store.getState());
